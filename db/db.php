997e82d7de1f4b35f13fe092e31e26bed9c73890
budnikov99 <budnikov.a.s@yandex.ru>
2021-07-29 13:58:56 +0400

    database placeholder

---
+++ db/db.php
@@ -0,0 +1,24 @@
+<?php
+define("ROOT", $_SERVER["DOCUMENT_ROOT"]);
+
+function get_json($name){
+    $content = file_get_contents(ROOT."/db/".$name);
+    if(!$content){
+        return null;
+    }
+    return json_decode($content);
+}
+
+function get_json_all($dir){
+    $files = array_diff(scandir(ROOT."/db/".$dir."/"), array('.', '..'));
+    $list = [];
+    foreach ($files as $file) {
+        $content = get_json($dir."/".$file);
+        if($content){
+            $list[] = $content;
+        }
+    }
+    return $list;
+}
+
+?>
